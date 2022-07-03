d81e1475ce56c66e80cc1c1a56cb771f39b2c02b
Walpurgisnatch <Kawemirs@gmail.com>
2022-07-03 14:24:54 +0700

    packages updated

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/dist/index.css'
 import 'nprogress/nprogress.css'
 import markdown from './directives/markdown';
 
